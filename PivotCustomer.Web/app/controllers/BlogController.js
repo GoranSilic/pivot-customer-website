65f61634585495f4c9dbe052c76d9621a2a63b1e
Goran Silic <goransilic11@gmail.com>
2017-08-14 19:59:15 +0200

    styling all pages

--- PivotCustomer.Web/app/controllers/BlogController.js
+++ PivotCustomer.Web/app/controllers/BlogController.js
@@ -1,3 +1,10 @@
 ﻿app.controller('BlogController', function ($rootScope, $scope, $state, $window, $timeout) {
+    $(".send-email input").on("change keyup paste", function () {
+        var emailLength = $(this).val().length;
 
+        if (emailLength == 0)
+            $(".send-button").hide();
+        else
+            $(".send-button").show();
+    });
 });
