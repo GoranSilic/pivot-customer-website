1e8e851ac6e89ce79e18dd993ca7636dc57bf8e3
Goran Silic <goransilic11@gmail.com>
2017-08-23 16:56:10 +0200

    styling about page

--- PivotCustomer.Web/app/controllers/AboutUsController.js
+++ PivotCustomer.Web/app/controllers/AboutUsController.js
@@ -1,3 +1,5 @@
 ﻿app.controller('AboutUsController', function ($rootScope, $scope, $state, $window, $timeout) {
-
+    $('#toggleAdvisors').click(function () {
+        $('.advisors-box').toggle();
+    });
 });
