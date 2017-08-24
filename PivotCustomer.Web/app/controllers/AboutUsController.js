bfc472dd63cb12cefda27ec946e876012f4602be
Goran Silic <goransilic11@gmail.com>
2017-08-24 22:52:54 +0200

    refactoring

--- PivotCustomer.Web/app/controllers/AboutUsController.js
+++ PivotCustomer.Web/app/controllers/AboutUsController.js
@@ -1,5 +1,24 @@
 ﻿app.controller('AboutUsController', function ($rootScope, $scope, $state, $window, $timeout) {
+
+
+
     $('#toggleAdvisors').click(function () {
-        $('.advisors-box').toggle();
+        $('.advisors-box').slideToggle();
+
+        $(this).toggleClass('opened');
+
+        if ($(this).hasClass('opened'))
+            $(this).text("Close Section");
+        else
+            $(this).text("View Advisors");
     });
+
+
+
+    function truncate(string) {
+        if (string.length > 50)
+            return string.substring(0, 50) + '...';
+        else
+            return string;
+    };
 });
